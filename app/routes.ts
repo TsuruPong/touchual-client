0076dd23e2e79094bc8116304be24576f2639ec2
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-05-10 21:17:37 +0900

    add: route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    layout("./routes/layout.tsx", [
+        index("./routes/top.tsx"),
+        route("countdown", "./routes/countdown.tsx"),
+        route("game", "./routes/game.tsx"),
+        route("result", "./routes/result.tsx")
+    ])
+] satisfies RouteConfig;
