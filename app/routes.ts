c6f6868eaa2d8aa06cb67977bcee8b4e4fb9df57
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-05-10 21:51:58 +0900

    fix: routing

--- app/routes.ts
+++ app/routes.ts
@@ -2,9 +2,9 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
     layout("./routes/layout.tsx", [
-        index("./routes/top.tsx"),
-        route("countdown", "./routes/countdown.tsx"),
-        route("game", "./routes/game.tsx"),
-        route("result", "./routes/result.tsx")
+        index("./routes/top/index.tsx"),
+        route("countdown", "./routes/countdown/index.tsx"),
+        route("game", "./routes/game/index.tsx"),
+        route("result", "./routes/result/index.tsx")
     ])
 ] satisfies RouteConfig;
