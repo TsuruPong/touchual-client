270b8d6f353dbad58a131f53924d3843978498f2
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-05-11 22:19:46 +0900

    add: countdown components

---
+++ app/routes/countdown/CountdownPresentation.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import * as React from "react";
+import { TimerKind, useTimer } from "~/hooks/useTimer";
+
+export const CountDownPresentation: React.FC<{ handleTimeUp: () => void }> = ({
+    handleTimeUp,
+}) => {
+    const { time, start } = useTimer(TimerKind.SUB, 3);
+    React.useEffect(() => {
+        start();
+    }, []);
+    React.useEffect(() => {
+        if (time <= 0) {
+            handleTimeUp();
+        }
+    }, [time]);
+    return (
+        <div className="flex justify-center items-center h-full">
+            <div className="mx-auto my-0">
+                <span>{time}</span>
+            </div>
+        </div>
+    );
+};
--- app/routes/countdown/index.tsx
+++ app/routes/countdown/index.tsx
@@ -1,9 +1,25 @@
-import type { LoaderFunction } from "react-router";
+import { useNavigate, type LoaderFunction } from "react-router";
+import { useKeyboardInput } from "~/hooks/useKeyboardInput";
+import { CountDownPresentation } from "./CountdownPresentation";
 
 export const loader: LoaderFunction = async () => {
     return null;
 };
 
 export default function Countdown() {
-    return <div>countdown</div>;
+    const nav = useNavigate();
+
+    useKeyboardInput((event: KeyboardEvent) => {
+        if (event.code == "Escape") backward();
+    });
+
+    const forward = () => {
+        nav("/game");
+    };
+
+    const backward = () => {
+        nav("/");
+    };
+
+    return <CountDownPresentation handleTimeUp={forward} />;
 }
