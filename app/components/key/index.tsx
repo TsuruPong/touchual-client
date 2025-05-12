d505fd8783e93233bcd94e36ee734fcf1c985113
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-05-12 22:47:57 +0900

    add: key component

---
+++ app/components/key/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+import { TextEn } from "../typography";
+
+import styles from "./Key.module.css";
+
+const Key: React.FC<{
+    size: "standard" | "regular" | "large" | "big" | "huge" | "space";
+    symbol: string;
+    code: string;
+    isPress: boolean;
+}> = ({ size, symbol, code, isPress }) => {
+    return (
+        <div
+            id={`code-${code}`}
+            className={`
+                ${styles.key}
+                ${styles[`${size}Key`]}
+                ${isPress ? styles.keyActive : ""}
+            `}
+        >
+            <TextEn>{symbol}</TextEn>
+        </div>
+    );
+};
+
+export default Key;
