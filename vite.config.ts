ba8c9e19b5c7ed55618f709bf4025ef90f9138a8
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-05-19 22:19:30 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: "/",
   css: {
     modules: {
       localsConvention: 'dashes',
