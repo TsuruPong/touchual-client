73a90f07063bdb729e5ad84fa7ff1b4fc738259a
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-06-14 01:05:17 +0900

    design

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { TextEn } from "../typography";
 
 export const Footer: React.FC = () => {
     return (
-        <div className="h-24 flex flex-col justify-center">
+        <div className="h-24 flex flex-col justify-center border-t-1 border-gray-500">
             <div className="h-[80%] flex justify-center">
                 <div className="w-[70%] h-full flex justify-between items-center">
                     <TextEn className="text-sm">
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -3,7 +3,7 @@ import { TextEn } from "../typography";
 
 export const Header: React.FC = () => {
     return (
-        <div className="h-24 p-1">
+        <div className="h-24 p-1 border-b-1 border-gray-500">
             <div className="flex h-full">
                 <img className="w-23 h-hull" src="/keyboard.svg" />
                 <TextEn className="text-3xl flex items-center">Touchual</TextEn>
